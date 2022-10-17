4ae8475cd8063c3a2a1121b047290e6dd35e3dbd
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-17 17:47:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+   const messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+const buyGifts = ["Guadalupe", "Ollie", "Aki"];
+console.log(writeCards(buyGifts, "surprise"));
+
+function countDown(number) {
+    while (number >= 0) {
+       console.log(number--);
+    }
+}
+countDown(10);
+
